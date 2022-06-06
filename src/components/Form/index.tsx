96e89dffbc0b2a1354361f84ed2ba19cac76d8b1
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-06-06 18:08:26 -0300

    putting state in the form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,14 +3,26 @@ import Button from "../Button";
 import style from './Form.module.scss';
 
 class Form extends React.Component {
+  state = {
+    tarefa: "",
+    tempo: "00:00"
+  }
+
+  adicionarTarefa(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("state: " , this.state)
+  }
+
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adcione um novo estudo</label>
           <input type="text"
           name="tarefa"
           id="tarefa"
+          value={this.state.tarefa}
+          onChange={ e => this.setState({...this.state, tarefa: e.target.value })}
           placeholder="O que você quer estudar?"
           required
           />
@@ -21,6 +33,8 @@ class Form extends React.Component {
           <input type="time"
           step="1"
           name="tempo"
+          value={this.state.tempo}
+          onChange={ e => this.setState({...this.state, tempo: e.target.value })}
           id="tempo"
           min="00:00:00"
           max="01:30:00"
