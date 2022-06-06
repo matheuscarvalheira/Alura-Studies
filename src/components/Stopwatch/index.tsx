aaf01c188f72be44ff3406bd406db9503d61eef5
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-06-06 15:18:31 -0300

    created stopwatch component

---
+++ src/components/Stopwatch/index.tsx
@@ -0,0 +1,17 @@
+import Button from "../Button";
+import Clock from './Clock';
+import style from './Stopwatch.module.scss';
+
+export default function Stopwatch(){
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o cronômetro!</p>
+      <div className={style.relogioWrapper}>
+        <Clock />
+      </div>
+      <Button>
+        Começar
+      </Button>
+    </div>
+  )
+}
