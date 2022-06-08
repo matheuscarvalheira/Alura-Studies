9f267e78458fe9617503ddb678c7969e1011a76e
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-06-07 21:22:27 -0300

    removing static values and importing interface

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,27 +1,13 @@
-import React, { useState } from "react";
+import { ITarefa } from "../../types/tarefa";
 import Item from "./Item/Index";
 import style from './List.module.scss';
 
-function List() {
-  const [tarefas, setTarefas] = useState( [
-    {
-      tarefa: "React",
-      tempo: "02:00:00",
-    },
-    {
-      tarefa: "Javascript",
-      tempo: "01:00:00",
-    },
-    {
-      tarefa: "Typescript",
-      tempo: "03:00:00",
-    }
-  ]);
+
+
+function List({tarefas}: {tarefas: ITarefa[]}) {
   return (
     <aside className={style.listaTarefas}>
-      <h2 onClick={() => {
-        setTarefas([...tarefas, {tarefa: 'Estudar Spring boot', tempo: '05:00:00'} ]);
-      }}>Estudos do Dia</h2>
+      <h2>Estudos do Dia</h2>
       <ul>
         {tarefas.map((item, index) =>(
           <Item 
