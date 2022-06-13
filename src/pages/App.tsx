5bdbc5329ccf49d00396d65979e0b4b547af95b6
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-06-13 19:48:36 -0300

    creating task select function for props

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,10 +7,19 @@ import style from "./App.module.scss";
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa){
+    setSelecionado(tarefaSelecionada);
+  }
+
   return (
     <div className={style.AppStyle}>
       <Form setTarefas={setTarefas}/>
-      <List tarefas={tarefas}/>
+      <List
+       tarefas={tarefas}
+       selecionaTarefa = {selecionaTarefa}
+       />
       <Stopwatch />
     </div>
   );
