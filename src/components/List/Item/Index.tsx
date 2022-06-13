6676d767cdcc45f390fb4a34f5ce4f16df94a885
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-06-13 19:48:08 -0300

    adding onClick event to item

--- src/components/List/Item/Index.tsx
+++ src/components/List/Item/Index.tsx
@@ -1,11 +1,24 @@
 import { ITarefa } from '../../../types/tarefa';
 import style from '../List.module.scss';
 
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
 
-function Item({tarefa, tempo, selecionado, completado, id} : ITarefa) {
+
+function Item({tarefa, tempo, selecionado, completado, id, selecionaTarefa} : Props) {
   console.log("Item atual: ", {tarefa, tempo, selecionado, completado, id})
   return (
-    <li  className={style.item}>
+    <li
+      className={style.item} 
+      onClick= {()=> { selecionaTarefa(
+      {
+        tarefa,
+        tempo,
+        selecionado,
+        completado,
+        id
+      })}}>
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
