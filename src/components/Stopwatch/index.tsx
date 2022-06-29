0a0a896ee3cc98cefee2aa3d6b4c52fc81e8cfa9
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2022-06-29 18:04:31 -0300

    finished regressive logic

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -6,24 +6,37 @@ import Clock from "./Clock";
 import style from "./Stopwatch.module.scss";
 
 interface Props {
-  selecionado: ITarefa | undefined;
+  selecionado: ITarefa | undefined,
+  finalizarTarefa: () => void
+  
 }
 
-export default function Stopwatch({ selecionado }: Props) {
+export default function Stopwatch({ selecionado, finalizarTarefa }: Props) {
   const [tempo, setTempo] = useState<number>();
 
   useEffect(() => {
-   if(selecionado?.tempo){
-    setTempo(tempoParaSegundos(selecionado.tempo));
-   }
-  }, [selecionado])
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado]);
+
+  function regressiva(contador: number = 0) {
+    setTimeout(() => {
+      if (contador > 0) {
+        setTempo(contador - 1);
+        return regressiva(contador - 1);
+      }
+      finalizarTarefa();
+    }, 1000);
+  }
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro!</p>
       <div className={style.relogioWrapper}>
-        <Clock tempo= {tempo} />
+        <Clock tempo={tempo} />
       </div>
-      <Button>Começar</Button>
+      <Button onClick={() => regressiva(tempo)}>Começar</Button>
     </div>
   );
 }
